cdec8acd27ce3b294278622baa61d2ac448eafa2
Michael West <michael@mwe.st>
2018-02-19 02:54:02 -0600

    Add beginning of .NET Core project using Nancy

---
+++ FlightsModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARHAB
+{
+    public class FlightsModule : NancyModule
+    {
+        public FlightsModule()
+        {
+            Get("/", args => "Hello World");
+        }
+    }
+}
